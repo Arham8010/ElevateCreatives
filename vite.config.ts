d5bf4c09545f854cb3d1e0dd656d832288a99289
Arham Aun Momin <mominarham39@gmail.com>
2026-01-01 14:35:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // This allows process.env.API_KEY to work in the browser, 
-    // pulling from Vercel's environment variables.
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  },
   build: {
     outDir: 'dist',
     sourcemap: false
